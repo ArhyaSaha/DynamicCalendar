95ec66a60358cff713ecabadd352c5d78c6236e8
ArhyaSaha <111699634+ArhyaSaha@users.noreply.github.com>
2024-12-13 10:04:49 +0530

    Update main.jsx
    Changing relative path for UserContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import UserContextProvider from './context/userContextProvider'
+import UserContextProvider from './context/UserContextProvider'
 import './index.css'
 import App from './App.jsx'
 
